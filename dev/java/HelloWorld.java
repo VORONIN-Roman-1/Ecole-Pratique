e200c676d5f4473bcf29ddc9de0ccc10f8647709
VORONIN Roman <voronin.roman.i@gmail.com>
2021-07-23 09:27:42 +0200

    Initial commit

---
+++ dev/java/HelloWorld.java
@@ -0,0 +1,20 @@
+import static java.lang.System.out;
+import java.io.PrintStream;
+
+public class HelloWorld{
+	public static void main (String[] args ){
+		PrintStream print = out;
+		
+		int age = 10;
+		String s="Je m'appelle Toto et j'ai " + age + " age.\n";
+		
+		print.print(s);
+		
+		/**print.print("Je m'appelle Toto et j'ai ");
+		print.print( age );
+		print.print(" age.");*/
+
+			
+
+	}
+}
