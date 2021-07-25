a4577acbf7efcd41da7afabd6db78387823da886
VORONIN Roman <voronin.roman.i@gmail.com>
2021-07-25 09:07:38 +0200

    Exercice3 Variable semaine 1

---
+++ Java/Variables/Exercice3.java
@@ -0,0 +1,34 @@
+public class Exercice3
+{
+	public static void main(String[] args) {
+		
+		// Corriger le code à partir d'ici
+		
+		int pieces = 2;
+		
+		System.out.println("J'ai un porte-monnaie");
+		
+		System.out.print("Voici le nombre de pieces que j'ai :");
+		
+		System.out.println(pieces);
+		
+		System.out.println("Je suis content de tout ca.");
+		
+		System.out.println("Je peux m'acheter ");
+		
+		int nb_pain_chocolat = 2;
+		
+		System.out.println(nb_pain_chocolat);
+
+		
+		System.out.println("Trop super");
+		
+		
+		// Fin de la partie du code erroné
+		// compilez avec : 
+		// javac Exercice3.java
+		// Exécutez avec : 
+		// java Exercice3
+		
+	}
+}
