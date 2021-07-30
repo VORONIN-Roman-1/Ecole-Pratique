2014033ae7501fd05e88646d6cb11258e67af940
VORONIN Roman <voronin.roman.i@gmail.com>
2021-07-30 12:36:38 +0200

    ToulEx5

--- dev/java/workspace26072021/ExercicesFonctions/src/exo/AffichBiTabl.java
+++ dev/java/workspace26072021/ExercicesFonctions/src/exo/AffichBiTabl.java
@@ -13,6 +13,7 @@ public class AffichBiTabl {
 			System.out.println("");
 		}
 		System.out.println("====================================");
+		
 		//Ex2
 		System.out.println(" EX2 ");
 		int min= m[0][0];
@@ -31,7 +32,10 @@ public class AffichBiTabl {
 		System.out.println("min = "+min+"\nmax = "+max+ "\nsomme = "+
 		somme+"\nmoyenne = "+ moyenne);
 		System.out.println("====================================");
+		
 		//Ex2 version2
+		System.out.println(" EX2 version 2 ");
+		
 		int minCol [] = new int [ m[0].length];
 		int maxCol [] = new int [ m[0].length];
 
@@ -42,6 +46,7 @@ public class AffichBiTabl {
 
 		int sommeCol[] = new int [m[0].length];
 		double moyenneCol[] = new double [m[0].length];
+		
 		for (int i=0; i<m.length; i++) {
 			for (int j=0; j<m[i].length; j++) {
 				minCol[j]=minCol[j]<m[i][j]?minCol[j]:m[i][j];
@@ -54,7 +59,52 @@ public class AffichBiTabl {
 			System.out.println("min colonne "+ i + " = "+minCol[i]+
 					"\nmax colonne " + i + " = " + maxCol[i]+ 
 					"\nsomme colonne "+ i + " = " + sommeCol[i]+
-					"\nmoyenne colonne "+ i + " = "+ moyenneCol[i]);
+					"\nmoyenne colonne "+ i + " = "+ moyenneCol[i]+ "\n");
 		}
+		System.out.println("====================================");
+		
+		//Ex3
+		System.out.println(" EX3");
+		int newM[][] = new int [m[0].length][m.length]; 
+		for (int i=0; i<m.length; i++) 
+			for (int j=0; j<m[i].length; j++) {
+				newM[j][i]= m[i][j];
+			}
+		for(int x[]:newM) {
+			for (int y : x)
+				System.out.print(y+" ");
+			System.out.println("");
+		}
+		System.out.println("====================================");
+		
+		//Ex4
+		System.out.println(" EX4");
+		int dixDix[][] = new int [10][10];
+		for (int i=0; i<dixDix.length; i++) 
+			for (int j=0; j<dixDix[i].length; j++) 
+				dixDix[i][j]=i*j;	
+		
+		for(int x[]:dixDix) {
+			for (int y : x)
+				System.out.print(y+" ");
+			System.out.println("");
+		}
+		System.out.println("====================================");
+		
+		//Ex5
+		System.out.println(" EX5");
+		System.out.println("Taille de tableau, SVP");
+		int taille = Recup.i(); 
+		int tailletaille[][] = new int [taille][taille];
+		for (int i=0; i<tailletaille.length; i++) 
+			for (int j=0; j<tailletaille[i].length; j++) 
+				tailletaille[i][j]=i*j;	
+		
+		for(int x[]:tailletaille) {
+			for (int y : x)
+				System.out.print(y+" ");
+			System.out.println("");
+		}
+		
 	}
 }
