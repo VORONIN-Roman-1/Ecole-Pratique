ae763ded0098c3c9ca6893e97292adf51671087d
VORONIN Roman <voronin.roman.i@gmail.com>
2021-08-11 21:05:05 +0200

    else if => case; amélioration pour une traduction; une standardisation

--- dev/java/workspace26072021/TP_Etudiant/src/execution/ExecutionPrincipale.java
+++ dev/java/workspace26072021/TP_Etudiant/src/execution/ExecutionPrincipale.java
@@ -12,37 +12,37 @@ public class ExecutionPrincipale {
 		do {
 			showMenuPrincipal();
 			input = Recup.i();
-
-			if (input == 1) {
+			switch (input) {
+			case 1:
 				showMenuChoix1();
 				for (Classe classe : classes) {
 					show(classe.affichageMoyenne());
 				}
-				show("");
-			}
-
-			else if (input == 2) {
+				showVideLine();
+				break;
+			case 2:
 				showMenuChoix2();
 				for (int i = 0; i < classes.length; i++) {
-					show((i + 1) + ") classe " + classes[i].nom);
+					showOneLine(String.valueOf(i + 1) + ")");
+					showClasse();
+					show(classes[i].nom);
 				}
-				show("0) Retour au menu principal");
+				showRetour();
+
 				int inputMenu2 = Recup.i();
 				if (inputMenu2 > 0 && inputMenu2 <= classes.length) {
 					show(classes[inputMenu2 - 1].affichage() + "\n");
 				}
-			}
-
-			else if (input == 3) {
+				break;
+			case 3:
 				showMenuChoix3();
 				for (Classe classe : classes) {
 					for (String etudiant : classe.tableauEtudiants())
 						show(etudiant);
 				}
-				show("");
-			}
-
-			else if (input == 4) {
+				showVideLine();
+				break;
+			case 4:
 				showMenuChoix4();
 				int countMenuChoix4 = 0;
 				for (int i = 0; i < classes.length; i++) {
@@ -51,7 +51,8 @@ public class ExecutionPrincipale {
 						show(countMenuChoix4 + ") " + classes[i].etudiants[j].nomPrenom());
 					}
 				}
-				show("0) Retour au menu principal");
+				showRetour();
+
 				int inputMenu4 = Recup.i();
 				countMenuChoix4 = 0;
 				for (int i = 0; i < classes.length; i++) {
@@ -61,56 +62,76 @@ public class ExecutionPrincipale {
 							show(classes[i].etudiants[j].affichage());
 					}
 				}
-				show("");
-			} else if (input == 5) {
+				showVideLine();
+				break;
+			case 5:
 				showMenuChoix5();
 				for (Classe classe : classes) {
 					show(classe.professeurPrincipal.nomPrenom());
 				}
-				show("");
+				showVideLine();
+				break;
+
 			}
+
 		} while (input != 0);
 
 	}
 
 	public static void show(String str) {
-		System.out.println(str);
+		showOneLine(str + "\n");
+	}
+
+	public static void showOneLine(String str) {
+		System.out.print(str);
+	}
+
+	public static void showRetour() {
+		show("0) Retour au menu principal");
+	}
+
+	public static void showClasse() {
+		showOneLine(" classe");
+	}
+
+	public static void showVideLine() {
+		show("");
 	}
 
 	public static void showMenuPrincipal() {
-		System.out.println("Menu Principal");
-		System.out.println("--------------");
-		System.out.println("1) Afficher les moyennes de chaque classe");
-		System.out.println("2) Afficher le détail d'une classe");
-		System.out.println("3) Afficher tous les étudiants");
-		System.out.println("4) Afficher le détail d'un étudiant");
-		System.out.println("5) Afficher tous les professeurs");
-		System.out.println("0) Quitter");
+		show("Menu Principal");
+		show("--------------");
+		show("1) Afficher les moyennes de chaque classe");
+		show("2) Afficher le détail d'une classe");
+		show("3) Afficher tous les étudiants");
+		show("4) Afficher le détail d'un étudiant");
+		show("5) Afficher tous les professeurs");
+		show("0) Quitter");
 	}
 
 	public static void showMenuChoix1() {
-		System.out.println("Moyennes de chaque classe");
-		System.out.println("-------------------------");
+		show("Moyennes de chaque classe");
+		show("-------------------------");
 	}
 
 	public static void showMenuChoix2() {
-		System.out.println("Afficher le détail d'une classe");
-		System.out.println("-------------------------------");
+		show("Afficher le détail d'une classe");
+		show("-------------------------------");
 	}
 
 	public static void showMenuChoix3() {
-		System.out.println("Tous les étudiants");
-		System.out.println("------------------");
+		show("Tous les étudiants");
+		show("------------------");
 	}
 
 	public static void showMenuChoix4() {
-		System.out.println("Afficher le détail d'un étudiant");
-		System.out.println("--------------------------------");
+		show("Afficher le détail d'un étudiant");
+		show("--------------------------------");
 	}
 
 	public static void showMenuChoix5() {
-		System.out.println("Afficher tous les professeurs");
-		System.out.println("-----------------------------");
+		show("Afficher tous les professeurs");
+		show("-----------------------------");
 	}
 
 }
