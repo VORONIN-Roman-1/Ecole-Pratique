67cd88fc30635ba64f4b8e23fa903613971cdcf2
VORONIN Roman <voronin.roman.i@gmail.com>
2021-09-30 23:15:26 +0200

    Domicile Spring MVC

---
+++ dev/java/workspace26072021/demo-04/WebContent/WEB-INF/view/salutation.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+Hello World of Spring!
+<br><br>
+User name is : ${param.name} 
+<br><br>
+The message: ${message}
+</body>
+</html>
---
+++ dev/java/workspace26072021/demo-04/src/com/mvcdemo/controller/DemoController.java
@@ -0,0 +1,30 @@
+package com.mvcdemo.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("hello")
+public class DemoController {
+	@GetMapping("/")
+	public String showHome() {
+		return "home";
+	}
+	@GetMapping ("getSalutationForm")
+	public String afficheSalutationForm() {
+		return  "salutation-form";
+	}
+	@GetMapping ("postSalutationForm")
+	public String traiteSalutationForm(@RequestParam("name") String nom, Model model) {
+		//String nom= request.getParameter("name");
+		String result = "Salut"+nom.toUpperCase(); 
+		model.addAttribute("message", result);
+		return  "salutation";
+	}
+	
+}
