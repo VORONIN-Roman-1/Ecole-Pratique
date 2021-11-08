e3cb0bc17d9999fe7817016f76c093c12c5a6a3a
VORONIN Roman <voronin.roman.i@gmail.com>
2021-11-08 16:13:02 +0100

    service Angular

---
+++ dev/Angular/HelloAngular/ng-service/src/app/employe/employe.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ListEmployeComponent } from './list-employe.component';
+import { DetailEmployeComponent } from './detail-employe.component';
+import { PourtourDirective } from './pourtour.directive';
+// import { EmployeRoleColorPipe } from './employe-role-color.pipe';
+import { EmployeService } from './employe.service';
+  
+@NgModule({
+    imports: [
+        CommonModule
+    ],
+    declarations: [
+        ListEmployeComponent,
+        DetailEmployeComponent,
+        PourtourDirective,
+        // EmployeRoleColorPipe
+    ],
+    providers: [EmployeService]
+})
+export class EmployeModule { }
