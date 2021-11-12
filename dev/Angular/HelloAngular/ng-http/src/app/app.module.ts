4699009e9e52a5dfdc94afc226c7478549366d65
VORONIN Roman <voronin.roman.i@gmail.com>
2021-11-12 16:25:27 +0100

    guard Angular

--- dev/Angular/HelloAngular/ng-http/src/app/app.module.ts
+++ dev/Angular/HelloAngular/ng-http/src/app/app.module.ts
@@ -7,17 +7,24 @@ import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './in-memory-data.service';
 
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './login.component';
+import { LoginRoutingModule } from './login-routing.module';
+
+
 
 
 @NgModule({
   declarations: [
-    AppComponent ,
-  ],
+    AppComponent , LoginComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {dataEncapsulation: false}),
     EmployeModule,
+    FormsModule,
+LoginRoutingModule,
+
     AppRoutingModule 
     
   ],
